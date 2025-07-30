8c5c9ac89b58db0619eaf9cd2c4d0d7130a376ea
Mwadena1000 <enockchakaya01@gmail.com>
2025-07-30 17:55:44 +0300

    added gitignore and jsonwebtoken to server

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 const cors = require('cors');
 
 const authRoutes = require('./routes/auth');
