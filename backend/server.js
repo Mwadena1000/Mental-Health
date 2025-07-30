907da46f50c386c6ea08306c9818adb7dc8e886a
Mwadena1000 <enockchakaya01@gmail.com>
2025-07-30 20:10:57 +0300

    app.use before routes

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-const authRoutes = require('./routes/auth');
-const quizRoutes = require('./routes/quiz');
-
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+const authRoutes = require('./routes/auth');
+const quizRoutes = require('./routes/quiz');
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.DB_URL /*, optional config object */)
